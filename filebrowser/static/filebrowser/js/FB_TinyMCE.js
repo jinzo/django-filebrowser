799bf6a462d9f998b7855bd3908046173ad23d75
Matjaž Črnko <matjaz.crnko@gmail.com>
2013-08-29 14:34:56 +0200

    Basic support for TinyMCE 4. Work in Progress.

--- filebrowser/static/filebrowser/js/FB_TinyMCE.js
+++ filebrowser/static/filebrowser/js/FB_TinyMCE.js
@@ -6,23 +6,23 @@ var FileBrowserDialogue = {
     },
     fileSubmit : function (FileURL) {
         var URL = FileURL;
-        var win = tinyMCEPopup.getWindowArg("window");
         
         // insert information now
-        win.document.getElementById(tinyMCEPopup.getWindowArg("input")).value = URL;
+        top.tinymce.activeEditor.windowManager.getParams().oninsert(URL);
         
-        // change width/height & show preview
+        // change width/height & show previewi
+/*
         if (win.ImageDialog){
             if (win.ImageDialog.getImageData)
                 win.ImageDialog.getImageData();
             if (win.ImageDialog.showPreviewImage)
                 win.ImageDialog.showPreviewImage(URL);
         }
-        
+  */      
         // close popup window
-        tinyMCEPopup.close();
+        top.tinymce.activeEditor.windowManager.close();
     }
 }
 
-tinyMCEPopup.onInit.add(FileBrowserDialogue.init, FileBrowserDialogue);
+//tinyMCEPopup.onInit.add(FileBrowserDialogue.init, FileBrowserDialogue);
 
